f84e3599452b069febfeca14803af3a002491984
Owen Mead-Robins <owenmead@gmail.com>
2016-02-01 21:25:07 -0800

    Move redux store into own module

--- index.js
+++ index.js
@@ -1,63 +1,9 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Redux from 'redux';
 
-const todo = (state, action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false
-      };
-
-    case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
-      return {
-        ...state,
-        completed: !state.completed,
-      };
-
-    default:
-      return state
-  }
-};
-
-const todos = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action)
-      ]
-    case 'TOGGLE_TODO':
-      return state.map(t => todo(t, action));
-    default:
-      return state;
-  }
-};
-
-const visibilityFilter = (state = 'SHOW_ALL', action) => {
-  switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter;
-    default:
-      return state;
-  }
-};
-
-import { combineReducers } from 'redux';
-const todoApp = combineReducers({
-  todos,
-  visibilityFilter
-});
-
-import { createStore } from 'redux';
-const store = createStore(todoApp);
-
-import { Component } from 'react';
+import { store } from './todoStore/store.js';
 
 const FilterLink = ({
   filter,
